2162517931938064eb2cd5c351ef0603e481089d
Mert <mertkosan@gmail.com>
2019-02-02 12:00:28 -0800

    new data generation model

--- data/MOCK/mock.py
+++ data/MOCK/mock.py
@@ -32,7 +32,7 @@ class DynamicRandomGraph:
         current_graph = first_graph
         for _ in range(1, count):
             next_graph = self._find_next_graph(current_graph)
-            if len(next_graph.edges) / len(current_graph.edges) >= 1 + self._event_trigger:
+            if self._is_event(next_graph, current_graph):
                 self._event_information.append(1)
                 self._reset_p_next(current_graph_density=nx.density(next_graph))
             else:
@@ -63,4 +63,54 @@ class DynamicRandomGraph:
     def _get_event_info(self):
         return self._event_information
 
-drg = DynamicRandomGraph(100)
+    def _is_event(self, next_graph, current_graph):
+        return len(next_graph.edges) / len(current_graph.edges) >= 1 + self._event_trigger
+
+
+class DynamicRandomGraph2:
+    def __init__(self, count):
+        self._graph_count = count  # Number of graph in dynamic graph
+
+        self._graphs = []  # All graphs.
+        self._n = 1000  # Initial number of nodes.
+        self._p = 0.01  # Initial probability of each edge.
+        self._p_next = 0.01  # Probability of an edge for next time.
+        self._event_trigger = 0.5  # How much increase density will be enough for an event.
+        self._p_change = 1  # Max percentage change of p_next for the next generation.
+
+        increase = True
+        self._event_information = []
+        for _ in range(0, count):
+            graph = nx.erdos_renyi_graph(self._n, self._p_next)
+            self._graphs.append(graph)
+            if self._is_event(graph):
+                self._event_information.append(1)
+                increase = False
+            else:
+                self._event_information.append(0)
+                if self._p_next <= self._p:
+                    increase = True
+            self._next_p_next(increase)
+
+    # reset p to default
+    def _reset_p_next(self):
+        self._p_next = self._p
+
+    # increase the probability of edge creation
+    def _next_p_next(self, increase=True):
+        if increase:
+            self._p_next += self._p_next * np.random.uniform(0, self._p_change)
+        else:
+            self._p_next -= self._p_next * np.random.uniform(0, self._p_change)
+
+    def _get_graphs(self):
+        return self._graphs
+
+    def _get_event_info(self):
+        return self._event_information
+
+    def _is_event(self, graph):
+        return nx.density(graph) >= self._event_trigger
+
+
+drg = DynamicRandomGraph2(100)
